766a3e87a9ec2ecffc9edcd3b26d171cb0ceec28
Matias <matias.haapasalmi@live.com>
2021-08-30 21:17:15 +0300

    Frontend added

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const axios = require("axios").default;
-
-let data = axios.get("https://dev-job-api.herokuapp.com/api/data/locations/tampere/technologies")
-.then(res => {
+let url = "https://dev-job-api.herokuapp.com/api/data/locations"
 
 
-    let array = (Object.entries(res.data))
+
+http.createServer( (request, response) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        "Content-Type" : "application/json"
+      };
+
+    console.log('request ', request.url);
+    let new_url = url + request.url + "/technologies";
+
+    axios.get(new_url).then(res => {
     
-
-    array.sort( ([key, value], [key2, value2]) => {
-        if (value > value2){
-            return -1;
-        }
-        else if (value < value2){
-            return 1;
-        }
+    
+        let array = (Object.entries(res.data))
         
-        return 0;
-        
-    });
+    
+        array.sort( ([key, value], [key2, value2]) => {
+            if (value > value2){
+                return -1;
+            }
+            else if (value < value2){
+                return 1;
+            }
+            
+            return 0;
+            
+        });
+    
+        response.writeHead(200, headers);
+        response.write(JSON.stringify(array));
+        response.end();
 
-    for (const [key, value] of array) {
-        console.log(`${key}: ${value}`);
+    }).catch(err => {
+        response.writeHead(400, headers);
+        response.write(JSON.stringify(err));
+        response.end();
+    })
 
-      }
-});
+}).listen(8080)
+
+
+// for (const [key, value] of array) {
+//             console.log(`${key}: ${value}`);
+    
+//           }
